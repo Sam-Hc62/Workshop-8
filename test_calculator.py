fc09be2c61bca9eb93128bc5450e14e5c88f5d9a
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-25 12:29:31 +0000

    7.1

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,13 @@ from calculator import square
 def main():
     test_square()
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print('untrue')
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print('untrue')
 if __name__ == "__main__":
     main()
