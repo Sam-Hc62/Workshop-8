0b0d5c8b5fb81477cd50dc1de4495b815fbe005b
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-25 12:39:16 +0000

    8.4

--- test_validator.py
+++ test_validator.py
@@ -3,3 +3,17 @@ from validator import is_valid_email
 def test_valid_emails():
     assert is_valid_email("student1@kent.ac.uk") == True
     assert is_valid_email("doctor@nhs.net") == True
+
+def test_invalid_emails():
+    try:
+        assert is_valid_email("hello@world") == False
+    except AssertionError:
+        print('error')
+    try:
+        assert is_valid_email("fake@kent.ac.uk.com") == False
+    except AssertionError:
+        print('error')
+    try:
+        assert is_valid_email("no_at_symbol.ac.uk") == False
+    except AssertionError:
+        print('error')
