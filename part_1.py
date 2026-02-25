e2e55653995a507fddb406da98a36bfdedfd5754
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-25 11:32:34 +0000

    1.3

--- part_1.py
+++ part_1.py
@@ -1,6 +1,8 @@
 email = input('what is your email...').strip()
-
+username, domain = email.split('@')
+if domain.endswith('.ac.uk'):
+    print('Valid domain')
 if '@' in email and '.' in email:
-    print('Valid')
+    print('Valid address')
 else:
     print('invalid')
