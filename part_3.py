3f52f03e7c86b35fc7d30527f5bfe2e3805ad129
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-25 11:51:26 +0000

    3.3

--- part_3.py
+++ part_3.py
@@ -1,4 +1,4 @@
 import re
 email = input('what is your email...').strip()
-if re.search(r'^.+[^@]@[^@].+\.ac.uk$', email):
+if re.search(r'^.+[a-zA-Z0-9_]@[a-zA-Z0-9_].+\.ac.uk$', email):
     print('valid')
