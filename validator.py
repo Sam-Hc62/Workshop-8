14a08e8a1cc13bb0eaaba8d91f1d127be87b58e9
Sam-Hc62 <sam.hcummins@gmail.com>
2026-02-25 12:33:05 +0000

    8.1

---
+++ validator.py
@@ -0,0 +1,13 @@
+import re
+def is_valid_email(email):
+    if re.search(r"^\w+@\w.+\.(ac.uk|gov.uk|nhs.net)$", email):
+        return True
+    return False
+def main():
+    email = input("What's your email? ").strip()
+    if is_valid_email(email):
+        print("Valid")
+    else:
+        print("Invalid")
+if __name__ == "__main__":
+    main()
